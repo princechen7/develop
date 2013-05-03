680c0bb5514f0816ff4cb56db9f857fcf92a2257
chenb <chenb@Bins-Pro.local>
2013-05-03 16:22:57 +0800

    dffffff

---
+++ Gittemp/CHBViewController2.h
@@ -0,0 +1,19 @@
+//
+//  CHBViewController2.h
+//  Gittemp
+//
+//  Created by chenb on 13-5-3.
+//  Copyright (c) 2013年 chenb. All rights reserved.
+//
+
+#import "CHBViewController.h"
+
+
+
+
+@interface CHBViewController2 : CHBViewController
+
+
+
+
+@end
