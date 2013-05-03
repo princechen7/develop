ad8a7d0ff4437e08faf6330d198f6a0ef5040d04
chenb <chenb@Bins-Pro.local>
2013-05-03 11:57:57 +0800

    Initial Commit

---
+++ Gittemp/CHBViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHBViewController.h
+//  Gittemp
+//
+//  Created by chenb on 13-5-3.
+//  Copyright (c) 2013年 chenb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHBViewController : UIViewController
+
+@end
