8ad0bb7c8248a929a143d5fee2ab62e3cbd2c2f8
chenb <chenb@Bins-Pro.local>
2013-05-03 18:13:53 +0800

    Initial Commit

---
+++ df/CHBViewController.m
@@ -0,0 +1,29 @@
+//
+//  CHBViewController.m
+//  df
+//
+//  Created by chenb on 13-5-3.
+//  Copyright (c) 2013年 chenb. All rights reserved.
+//
+
+#import "CHBViewController.h"
+
+@interface CHBViewController ()
+
+@end
+
+@implementation CHBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ df/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  df
+//
+//  Created by chenb on 13-5-3.
+//  Copyright (c) 2013年 chenb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CHBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CHBAppDelegate class]));
+    }
+}
